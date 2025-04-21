8824ac92d59299aa969c7a0c7b5a41a3a31dc96f
kimsongie12 <song3078@naver.com>
2025-04-21 21:22:40 +0900

    문제 19) 문자반복출력하기 3weeks coding questions 입문 문제

---
+++ src/introduction100/문자반복출력하기/q19.java
@@ -0,0 +1,26 @@
+package introduction100.문자반복출력하기;
+// 시간 복잡도 O(1)
+// Math.sqrt() : 재곱근 구하는 메서드
+public class q19 {
+    public int solution(int n) {
+        /* 4 = 2 2
+          16 = 4 4
+        * */
+        int num = (int) Math.sqrt(n);
+        if( num * num == n){
+            return 1;
+
+        }else {
+            return 2;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        q19 num = new q19();
+        int n = 976;
+        int res = num.solution(n);
+        System.out.println(res);
+    }
+}
