3bf7ce4d3f593e6093d15b0c292f5e332647791d
kimsongie12 <song3078@naver.com>
2025-09-14 19:07:45 +0900

    사칙연산 완료

---
+++ src/backjoon/고양이/B10171.java
@@ -0,0 +1,16 @@
+package backjoon.고양이;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10171 {
+    public static void main(String[] args)  {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
